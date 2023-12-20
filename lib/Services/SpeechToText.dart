5e524fadfdac9b2cee9747f51bb0839f8f942e3b
Balaji-V-S <vsbrobot@gmail.com>
2023-12-20 14:04:01 +0530

    Added Lock for Bot page

--- lib/Services/SpeechToText.dart
+++ lib/Services/SpeechToText.dart
@@ -1,21 +1,20 @@
-// ignore_for_file: unused_field
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 import 'package:langchain/langchain.dart';
 import 'package:langchain_openai/langchain_openai.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 
 const gptKey = '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';
 final llm = OpenAI(apiKey: gptKey);
-final model = ChatOpenAI(apiKey: gptKey, temperature: 0.5,model: 'gpt-3.5-turbo-1106');
+final model =
+    ChatOpenAI(apiKey: gptKey, temperature: 0.5, model: 'gpt-3.5-turbo-1106');
 const stringOutputParser = StringOutputParser();
 final memory = ConversationBufferMemory(returnMessages: true);
-
 
 final promptTemplate = ChatPromptTemplate.fromPromptMessages([
   SystemChatMessagePromptTemplate.fromTemplate(
@@ -48,6 +47,8 @@ class QueryModel extends StatefulWidget {
 class _QueryModelState extends State<QueryModel> {
   final SpeechToText _speechToText = SpeechToText();
 
+  bool isLocked = false;
+  String correctPassword = "";
   bool _speechEnabled = false;
   String _wordsSpoken = "";
   String llmResponse = "";
@@ -61,6 +62,11 @@ class _QueryModelState extends State<QueryModel> {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.bottom]);
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() {
+        correctPassword = prefs.getString('password')!;
+      });
+    });
     initSpeech();
     initTTS();
   }
@@ -91,25 +97,18 @@ class _QueryModelState extends State<QueryModel> {
   }
 
   Future<void> _callLLM(prompt) async {
+    prompt = 'Hello';
     print("reached function call");
     setState(() {
       lottiePath = "assets/animations/loading.json";
     });
+    final llmResponse = await chain.invoke(prompt);
     print(prompt + ":" + llmResponse);
-    const input1 = 'Hi, I am Bob';
-    final output1 = await chain.invoke(input1);
-    print(output1);
-// Hello Bob! How can I assist you today?
 
     await memory.saveContext(
-      inputValues: {'input': input1},
-      outputValues: {'output': output1},
+      inputValues: {'input': prompt},
+      outputValues: {'output': llmResponse},
     );
-
-    const input2 = "What's my name?";
-    final output2 = await chain.invoke(input2);
-    print(output2);
-// Your name is Bob, as you mentioned earlier.
     print("Context Saved");
     play(llmResponse);
   }
@@ -143,81 +142,212 @@ class _QueryModelState extends State<QueryModel> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(1, 13, 13, 13),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Lottie.asset(lottiePath, height: 375),
-            ),
-            GestureDetector(
-              onTap:
-                  _speechToText.isListening ? _stopListening : _startListening,
-              child: AvatarGlow(
-                glowColor: Colors.blue,
-                endRadius: 50.0,
-                duration: const Duration(milliseconds: 4500),
-                repeat: true,
-                showTwoGlows: true,
-                repeatPauseDuration: const Duration(milliseconds: 100),
-                child: Material(
-                  // Replace this child with your own
-                  elevation: 8.0,
-                  shape: const CircleBorder(),
-                  child: CircleAvatar(
-                    backgroundColor: Colors.grey[100],
-                    radius: 30.0,
-                    child: Image.asset(
-                      'assets/Images/mic.png', //replace it with the microhone image or icon
-                      height: 120,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            _speechToText.isListening
-                ? LottieBuilder.asset('assets/animations/micInitialized.json')
-                : const Padding(
-                    padding: EdgeInsets.only(bottom: 15),
-                    child: Text(
-                      "Not Initialised",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  llmResponse,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ),
-            ),
-            if (_speechToText.isNotListening && _confidenceLevel > 0)
+    return WillPopScope(
+      onWillPop: () async {
+        return !isLocked;
+      },
+      child: Scaffold(
+        backgroundColor: const Color.fromARGB(1, 13, 13, 13),
+        body: Center(
+          child: Column(
+            children: <Widget>[
               Padding(
-                padding: const EdgeInsets.only(
-                  bottom: 100,
-                ),
-                child: Text(
-                  "Confidence: ${(_confidenceLevel * 100).toStringAsFixed(1)}%",
-                  style: const TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w200,
+                padding: const EdgeInsets.all(30),
+                child: Lottie.asset(lottiePath, height: 375),
+              ),
+              GestureDetector(
+                onTap: _speechToText.isListening
+                    ? _stopListening
+                    : _startListening,
+                child: AvatarGlow(
+                  glowColor: Colors.blue,
+                  endRadius: 50.0,
+                  duration: const Duration(milliseconds: 4500),
+                  repeat: true,
+                  showTwoGlows: true,
+                  repeatPauseDuration: const Duration(milliseconds: 100),
+                  child: Material(
+                    // Replace this child with your own
+                    elevation: 8.0,
+                    shape: const CircleBorder(),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey[100],
+                      radius: 30.0,
+                      child: Image.asset(
+                        'assets/Images/mic.png', //replace it with the microhone image or icon
+                        height: 120,
+                      ),
+                    ),
                   ),
                 ),
               ),
-          ],
+              _speechToText.isListening
+                  ? LottieBuilder.asset('assets/animations/micInitialized.json')
+                  : const Padding(
+                      padding: EdgeInsets.only(bottom: 15),
+                      child: Text(
+                        "Not Initialised",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    llmResponse,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ),
+              ),
+              if (_speechToText.isNotListening && _confidenceLevel > 0)
+                Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 100,
+                  ),
+                  child: Text(
+                    "Confidence: ${(_confidenceLevel * 100).toStringAsFixed(1)}%",
+                    style: const TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w200,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.large(
+          backgroundColor: Colors.white,
+          elevation: 50,
+          onPressed: () {
+            if (isLocked) {
+              _showPasswordDialog(context);
+            } else {
+              setState(() {
+                isLocked = !isLocked;
+              });
+              Fluttertoast.showToast(
+                msg: "Screen Locked",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIosWeb: 1,
+                backgroundColor: const Color.fromARGB(144, 255, 255, 255),
+                textColor: Colors.black,
+                fontSize: 16.0,
+              );
+            }
+          },
+          child: isLocked
+              ? const Icon(
+                  Icons.lock_outline,
+                  color: Colors.black,
+                  size: 40,
+                )
+              : const Icon(
+                  Icons.lock_open,
+                  color: Colors.black,
+                  size: 40,
+                ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => stop(),
-        child: const Icon(Icons.stop_circle_outlined),
-      ),
+    );
+  }
+
+  Future<void> _showPasswordDialog(BuildContext context) {
+    String enteredPassword = "";
+    bool isPasswordCorrect = false;
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: const Color.fromARGB(150, 13, 13, 13),
+          title: const Text(
+            'Enter Password',
+            style: TextStyle(color: Colors.white, fontFamily: 'Roboto'),
+          ),
+          content: TextField(
+            decoration: InputDecoration(
+              hintStyle: TextStyle(color: Colors.grey[400]),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              hintText: 'Password',
+            ),
+            style: const TextStyle(color: Colors.white),
+            onChanged: (value) {
+              enteredPassword = value;
+            },
+            obscureText: true,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false); // Close the dialog
+              },
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.white, fontFamily: 'Roboto'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                isPasswordCorrect = enteredPassword == correctPassword;
+
+                if (isPasswordCorrect) {
+                  // Unlock the page and close the dialog
+                  Navigator.of(context).pop(true);
+                  setState(() {
+                    isLocked = false;
+                  });
+                  Fluttertoast.showToast(
+                      msg: "Screen Unlocked",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: const Color.fromARGB(144, 255, 255, 255),
+                      textColor: Colors.black,
+                      fontSize: 16.0);
+                } else {
+                  // Show error message for wrong password
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        backgroundColor: const Color.fromARGB(150, 13, 13, 13),
+                        title: const Text(
+                          'Wrong Password',
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Roboto'),
+                        ),
+                        content: const Text(
+                          'Please try again.',
+                          style: TextStyle(
+                              color: Colors.white, fontFamily: 'Roboto'),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              child: const Text(
+                'Unlock',
+                style: TextStyle(color: Colors.white, fontFamily: 'Roboto'),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
